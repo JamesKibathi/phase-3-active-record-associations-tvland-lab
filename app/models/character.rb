1666e19539855710f9dea00a164f8c8419b2d0d4
James Njenga <knjenga94@gmail.com>
2022-11-07 02:23:44 +0300

    First attempt

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,11 @@
 class Character < ActiveRecord::Base
-  
+  belongs_to :actor_id
+  belongs_to :show
+  def say_that_thing_you_say
+    "#{self.name} always says: #{self.catchphrase}"
+  end
+
+#   def show
+#     self.show
+#   end
 end
